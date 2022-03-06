d5b6fa8bb9673fba70a1b2b9e44f721fcfa708e6
Léo Cheffi <contact@leocheffi.com>
2022-03-06 14:07:49 +0100

    make the 2 rocket missile left and right but not yet fixed in the main spaceship

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const ctx = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+// clear the canvas (not repeat img after mouvement)
 function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
   }
@@ -88,12 +89,81 @@ const key = {
     q: {pressed: false},
     space: {pressed: false}
 }
+// creation right rocket 
+class Missile1{
+    constructor(){
+        const ref = 0.14
+        const image = new Image()
+        image.src = "./images/MissileRight.png"
+        image.onload = () =>{
+            this.image = image
+            this.width = image.width * ref
+            this.height = image.height * ref
+            this.position = {
+                x: canvas.width/2 - this.width + 120,
+                y: canvas.height - this.height - 130
+            };
+        }
+        this.speed = {
+            x: 0,
+            y: 0
+        }  
+    }
+    draw(){
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    }
+    refresh(){
+        if(this.image){
+            this.draw()
+            this.position.x += this.speed.x
+            this.position.y += this.speed.y
+        }
+    }
+}
+const missile1 = [new Missile1()]
 
 
+// creation left rocket 
+class Missile2{
+    constructor(){
+        const ref = 0.14 
+        const image = new Image()
+        image.src = "./images/MissileLeft.png"
+        image.onload = () =>{
+            this.image = image
+            this.width = image.width * ref
+            this.height = image.height * ref
+            this.position = {
+                x: canvas.width/2 - this.width + 120,
+                y: canvas.height - this.height - 130
+            };
+        }
+        this.speed = {
+            x: 0,
+            y: 0
+        }  
+    }
+    draw(){
+        ctx.drawImage(this.image, this.position.x, this.position.y, this.width, this.height);
+    }
+    refresh(){
+        if(this.image){
+            this.draw()
+            this.position.x += this.speed.x
+            this.position.y += this.speed.y
+        }
+    }
+}
+const missile2 = [new Missile2()]
+
+// here is the engine of the game =)
 animation = () => {
     requestAnimationFrame(animation)
     player.refresh();
     ufo.refresh();
+    missile1.forEach((rocket)=> rocket.refresh())
+    missile2.forEach((rocket)=> rocket.refresh())
+    
     if((key.ArrowRight.pressed || key.d.pressed) && player.position.x + player.width <= canvas.width )
         player.speed.x = 9 
     else if((key.ArrowLeft.pressed || key.q.pressed ) && player.position.x >= 0)
